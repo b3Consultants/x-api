02444046195179fe73ebfebf64bc13187e8fd5f0
Agustin <elmaco92@gmail.comm>
2017-05-05 19:53:42 -0300

    Develoment Branch

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ var mongoose = require('mongoose'); 				// mongoose for mongodb
 var port = process.env.PORT || 8080; 				// set the port
 var database = require('./config/database'); 			// load the database config
 var morgan = require('morgan');
-var nev = require('email-verification')(mongoose);
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var fs = require('fs');
@@ -28,7 +27,7 @@ app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-M
 
 
 // routes ======================================================================
-require('./app/routes.js')(app,nev);
+require('./app/routes.js')(app);
 
 // listen (start app with node server.js) ======================================
 app.listen(port);
